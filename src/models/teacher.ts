ec85cfbdc99f39d1046d3aadcaef96cd093bf21c
Deepak Shaw <shawdeepak185@gmail.com>
2021-05-05 12:30:38 +0530

    model correction

--- src/models/teacher.ts
+++ src/models/teacher.ts
@@ -1,3 +1,11 @@
+import { IStandard } from "./standard";
+import { Subject } from "./subject";
+
 export interface ITeacher {
+    id:number;
+    name:string;
+    standard:IStandard;
+    standardList:IStandard[];
+    subject : Subject;
     
 }
