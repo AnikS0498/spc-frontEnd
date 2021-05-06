f4b4c5f99c53fdce465202f9c78f90ebb580021d
P08Rahul <p08rahul@gmail.com>
2021-05-06 20:12:06 +0530

    admin module

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,19 +3,22 @@ import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin-component/admin.component';
 import { RouterModule } from '@angular/router';
 import { AdminGetAllParentsComponent } from './admin-get-all-parents/admin-get-all-parents.component';
+import { AdminGetAllTeachersComponent } from './admin-get-all-teachers/admin-get-all-teachers.component';
 
 
 
 @NgModule({
   declarations: [
     AdminComponent,
-    AdminGetAllParentsComponent
+    AdminGetAllParentsComponent,
+    AdminGetAllTeachersComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild([
       { path:'admin', component:AdminComponent },
-      { path:'admin/getAllParents', component:AdminGetAllParentsComponent }
+      { path:'admin/getAllParents', component:AdminGetAllParentsComponent },
+      { path:'admin/getAllTeachers', component:AdminGetAllTeachersComponent }
     ])
   ]
 })
